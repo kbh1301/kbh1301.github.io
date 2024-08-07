a2fc8d1397b387b0057fd6fd28aad196ac955043
Kyle Hulvey <83362164+kbh1301@users.noreply.github.com>
2024-08-07 16:19:04 -0500

    Fix navbar styles

--- src/lib/components/nav/main-nav.svelte
+++ src/lib/components/nav/main-nav.svelte
@@ -3,6 +3,8 @@
 	import { page } from "$app/stores";
 	import { siteConfig } from "$lib/config/site";
 	import { cn } from "$lib/utils";
+
+    $: currentHash = `${base}/${$page.state.hash}`;
 </script>
 
 <div class="hidden md:flex w-full justify-between">
@@ -17,14 +19,14 @@
 				href={navItem.href}
 				class={cn(
 					"py-1 transition-colors hover:text-primary",
-					$page.state.hash === navItem.href
+					currentHash === navItem.href
 						? "text-primary border-t-2 border-primary"
 						: "text-foreground"
 				)}
 				target={navItem.external ? "_blank" : undefined}
 				rel={navItem.external ? "noreferrer" : undefined}
 			>
-				{navItem.title}
+                {navItem.title}
 			</a>
 		{/each}
 	</nav>
