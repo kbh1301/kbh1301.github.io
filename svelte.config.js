afd19cd24716c432f9fb78abe96f3d0e8794988f
Kyle Hulvey <kyle.hulvey@gmail.com>
2025-12-07 22:23:23 -0600

    Build error fix

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,12 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+        prerender: {
+            handleHttpError: ({ status, path }) => {
+                if (status === 404 && path.endsWith('.pdf')) return 'continue';
+                return 'fail';
+            }
+        },
         paths: {
             base: process.env.NODE_ENV === 'production' ? '/kbh1301.github.io' : '',
         },
